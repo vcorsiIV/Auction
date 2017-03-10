9a950e60f84e6e59330c154f17723acdf1db4dfe
vcorsiIV <S01160644@acad.tri-c.edu>
2017-03-10 11:07:22 -0500

    Bonus Auction Program Submit

---
+++ Auction/Auction/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Auction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.intRadButton1 = new System.Windows.Forms.RadioButton();
+            this.dblRadButton = new System.Windows.Forms.RadioButton();
+            this.strRadButton = new System.Windows.Forms.RadioButton();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.currentBidLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(12, 72);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(222, 20);
+            this.txtBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(264, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // intRadButton1
+            // 
+            this.intRadButton1.AutoSize = true;
+            this.intRadButton1.Checked = true;
+            this.intRadButton1.Location = new System.Drawing.Point(12, 109);
+            this.intRadButton1.Name = "intRadButton1";
+            this.intRadButton1.Size = new System.Drawing.Size(107, 17);
+            this.intRadButton1.TabIndex = 3;
+            this.intRadButton1.TabStop = true;
+            this.intRadButton1.Text = "int (Pay by Credit)";
+            this.intRadButton1.UseVisualStyleBackColor = true;
+            this.intRadButton1.CheckedChanged += new System.EventHandler(this.radButton1_CheckedChanged);
+            // 
+            // dblRadButton
+            // 
+            this.dblRadButton.AutoSize = true;
+            this.dblRadButton.Location = new System.Drawing.Point(12, 158);
+            this.dblRadButton.Name = "dblRadButton";
+            this.dblRadButton.Size = new System.Drawing.Size(125, 17);
+            this.dblRadButton.TabIndex = 4;
+            this.dblRadButton.Text = "double (Pay by Cash)";
+            this.dblRadButton.UseVisualStyleBackColor = true;
+            this.dblRadButton.CheckedChanged += new System.EventHandler(this.radButton2_CheckedChanged);
+            // 
+            // strRadButton
+            // 
+            this.strRadButton.AutoSize = true;
+            this.strRadButton.Location = new System.Drawing.Point(12, 213);
+            this.strRadButton.Name = "strRadButton";
+            this.strRadButton.Size = new System.Drawing.Size(125, 17);
+            this.strRadButton.TabIndex = 5;
+            this.strRadButton.Text = "string (Pay by Check)";
+            this.strRadButton.UseVisualStyleBackColor = true;
+            this.strRadButton.CheckedChanged += new System.EventHandler(this.radButton3_CheckedChanged);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(234, 109);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(73, 13);
+            this.lblResult.TabIndex = 6;
+            this.lblResult.Text = "Your bid here.";
+            // 
+            // currentBidLbl
+            // 
+            this.currentBidLbl.AutoSize = true;
+            this.currentBidLbl.Location = new System.Drawing.Point(461, 293);
+            this.currentBidLbl.Name = "currentBidLbl";
+            this.currentBidLbl.Size = new System.Drawing.Size(97, 13);
+            this.currentBidLbl.TabIndex = 7;
+            this.currentBidLbl.Text = "Current bid:   $0.00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Please Select the method of which you wish to bid.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(259, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "We only accept bids over the value of $10.00 dollars.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(424, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "If you wish to write a check enter in either $XX.XX or XX.XX dollars for a valid " +
+    "acception.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 315);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.currentBidLbl);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.strRadButton);
+            this.Controls.Add(this.dblRadButton);
+            this.Controls.Add(this.intRadButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton intRadButton1;
+        private System.Windows.Forms.RadioButton dblRadButton;
+        private System.Windows.Forms.RadioButton strRadButton;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label currentBidLbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Auction/Auction/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Auction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        int choice = 1;
+        double currentBid = 0;
+        
+
+        private void radButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            //choosing to submit a int
+            currentBidLbl.Text = "";
+            choice = 1;
+            currentBidLbl.Text = String.Format("Current bid:   ") + currentBid.ToString("C");
+        }
+
+        private void radButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            //choosing to submit a double
+            currentBidLbl.Text = "";
+            choice = 2;
+            currentBidLbl.Text = String.Format("Current bid:   ") + currentBid.ToString("C");
+        }
+
+        private void radButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            //choosing to submit a string
+            currentBidLbl.Text = "";
+            choice = 3;
+            currentBidLbl.Text = String.Format("Current bid:   ") + currentBid.ToString("C");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int intVal;
+            double dblVal;
+            string input, strVal;
+            input = txtBox1.Text;
+
+            //conversion
+            if (choice == 1)
+            {
+                try
+                {
+                    string message;
+                    intVal = Convert.ToInt32(input);
+                    currentBid = Bidding(intVal, out message, currentBid);
+                    lblResult.Text = String.Format(message);
+                    currentBidLbl.Text = String.Format("Current bid:   ") + currentBid.ToString("C");
+
+                }
+                catch
+                {
+                    lblResult.Text = String.Format("Error: {0}\nEnter in an integer.", input);
+                }
+            }
+
+            else if(choice == 2)
+            {
+                try
+                {
+                    string message;
+                    dblVal = Convert.ToDouble(input);
+                    currentBid = Bidding(dblVal, out message, currentBid);
+                    lblResult.Text = String.Format(message);
+                    currentBidLbl.Text = String.Format("Current bid:   ") + currentBid.ToString("C");
+                }
+                catch
+                {
+                    lblResult.Text = String.Format("Error: {0}\nEnter in an double.", input);
+                }
+
+                
+            }
+
+            else if (choice == 3)
+            {
+                string message;
+                strVal = "";
+
+                char [] charArr = new char[input.Length + 7];
+                charArr = input.ToCharArray();
+                try
+                {
+                    if (charArr[0] == '$')
+                    {
+                        for (int i = 1; i < input.Length; i++)
+                        {
+                            strVal += charArr[i];
+                        }
+                        strVal.Trim();
+
+                        currentBid = Bidding(strVal, out message, currentBid);
+                        lblResult.Text = String.Format(message);
+                        currentBidLbl.Text = String.Format("Current bid:   ") + currentBid.ToString("C");
+
+                    }
+                    // x-7  x-6   x-5   x-4  x-3  x-2   x-1
+                    // D     o     l     l    a    r    s
+                    else if (input.Contains("dollars") && charArr[input.Length - 1] == 's')
+                    {
+                        for (int i = 0; i < input.Length - 7; i++)
+                        {
+                            strVal += charArr[i];
+                        }
+                        strVal.Trim();
+
+                        currentBid = Bidding(strVal, out message, currentBid);
+                        lblResult.Text = String.Format(message);
+                        currentBidLbl.Text = String.Format("Current bid:   ") + currentBid.ToString("C");
+                    }
+                    else
+                    {
+                        lblResult.Text = String.Format("Please enter the string in a correct format, as $X.XX or X.XX dollars\n Remember only values greater then $10 accepted.");
+                    }
+                }
+                
+                catch
+                {
+                    lblResult.Text = String.Format("Please enter the string in a correct format, as $X.XX or X.XX dollars\n Remember only values greater then $10 accepted.");
+                }
+            }
+
+        }
+
+        private static double Bidding(int inVal, out string str, double outVal)
+        {
+            if (inVal >= 10)
+            {
+                outVal += Convert.ToDouble(inVal);
+                double messageVal = Convert.ToDouble(inVal);
+                str = ("You have entered in " + messageVal.ToString("C") +
+                    ",\nIt is over the minimum bid of $10.00.\n The Current Bid is now " + outVal.ToString("C"));
+                return outVal;
+            }
+            else
+            {
+                str = ("You have entered in " + inVal.ToString("C") +
+                    ",\nIt is under the minimum bid of $10.\n The Current Bid remains at " + outVal.ToString("C"));
+                return outVal;
+            }
+        }
+
+        private static double Bidding(double inVal, out string str, double outVal)
+        {
+            if (inVal >= 10)
+            {
+                outVal += inVal;
+                str = ("You have entered in " + inVal.ToString("C") +
+                    ",\nIt is over the minimum bid of $10.\n The Current Bid is now " + outVal.ToString("C"));
+                return outVal;
+            }
+            else
+            {
+                str = ("You have entered in " + inVal.ToString("C") +
+                    ",\nIt is under the minimum bid of $10.\n The Current Bid remains at " + outVal.ToString("C"));
+                return outVal;
+            }
+        }
+
+        private static double Bidding(string inVal, out string str, double outVal)
+        {
+            double strValue = Convert.ToDouble(inVal);
+
+            if (strValue >= 10)
+            {
+                outVal += strValue;
+                str = ("You have entered in " + strValue.ToString("C").Trim() +
+                    ",\nIt is over the minimum bid of $10. The Current Bid is now " + outVal.ToString("C"));
+                return outVal;
+            }
+            else
+            {
+                str = ("You have entered in " + strValue.ToString("C").Trim() +
+                    ",\nIt is under the minimum bid of $10. the Current Bid remains at " + outVal.ToString("C"));
+                return outVal;
+            }
+        }
+
+    }
+}
